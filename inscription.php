585ef4b0821177d7bf8735b3497aa7be77b0900f
Arthur Babinot <ababinot@dombtsig.local>
2022-09-07 14:55:02 +0200

    ajout inscription.php

---
+++ inscription.php
@@ -0,0 +1,21 @@
+<html>
+    <head>
+       <meta charset="utf-8">
+        <!-- importer le fichier de style -->
+        <link rel="stylesheet" href="style.css" media="screen" type="text/css" />
+    </head>
+    <body>
+        <div id="container">
+            <!-- zone de connexion -->
+            
+            <form action="verification.php" method="POST">
+                <h1>Inscription</h1>
+                
+                
+                <?php
+                // Code de vérification 
+                ?>
+            </form>
+        </div>
+    </body>
+</html>
